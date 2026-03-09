106bc9955df83ca6a1c2d447928d7d83e6ea0be9
CarljNueva <qcjenueva@tip.edu.ph>
2026-03-09 20:20:34 +0800

    3/9/26 Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/aboutus_screen.dart';
 import 'screens/login_screen.dart';
-import 'screens/menu_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,8 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Code Shield',
       theme: ThemeData(
+        fontFamily: 'Pixelify',
+        brightness: Brightness.dark,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.grey.shade700,
+          seedColor: Colors.grey,
           brightness: Brightness.dark,
         ),
       ),
@@ -25,52 +26,116 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainNav extends StatefulWidget {
+class MainNav extends StatelessWidget {
   const MainNav({super.key});
-
-  @override
-  State<MainNav> createState() => _MainNavState();
-}
-
-class _MainNavState extends State<MainNav> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    LoginScreen(),
-    MenuScreen(),
-    AboutUsScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.login),
-            label: 'Login',
+      body: Stack(
+        children: [
+
+          // Background Image
+          SizedBox.expand(
+            child: Image.asset(
+              "assets/images/CodeShieldBackground.png",
+              fit: BoxFit.cover,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Menu',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About Us',
-          ),
+
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+                  Center(
+                    child: Image.asset(
+                      "assets/images/CodeShieldTitle.png",
+                      width: 800,
+                    ),
+                  ),
+
+                  const SizedBox(height: 80),
+
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("PLAY", 
+                                style: 
+                                TextStyle(fontSize: 30, color: Colors.white),
+                                ),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("HOW TO PLAY",
+                                  style: TextStyle(fontSize: 30, color: Colors.white)),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("ENEMY DESCRIPTION",
+                                  style: TextStyle(fontSize: 30, color: Colors.white)),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text("ABOUT",
+                                  style: TextStyle(fontSize: 30, color: Colors.white)),
+                            ),
+                          ],
+                        ),
+
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+
+                            const SizedBox(height: 125),
+                            
+                            Row(
+                              children: [
+                            const Text("GUEST",
+                                    style: TextStyle(fontSize: 30)),
+                                    IconButton(
+                                      icon: const Icon(Icons.edit),
+                                      onPressed: () {},
+                                    )
+                                  ],
+                                ),
+
+                            const SizedBox(height: 20),
+
+                            const Text(
+                              "HIGHSCORE: ",
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
